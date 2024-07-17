60ee111dfc0b4d0179312efc69da0eda9062b585
DiegoDiaz <diegodiazmed93@gmail.com>
2024-07-17 16:44:06 -0400

    primer webscrapper

--- web_scrapper.py
+++ web_scrapper.py
@@ -9,5 +9,8 @@ soup= BeautifulSoup(content,'lxml')
 
 box = soup.find('article',class_='main-article')
 title = box.find('h1').get_text()
-transcript = box.find('div',class_='full-script').get_text()
+transcript = box.find('div',class_='full-script').get_text(strip=True, separator=' ')
 print(title,transcript)
+
+with open(f'{title}.txt','w') as file:
+    file.write(transcript)
