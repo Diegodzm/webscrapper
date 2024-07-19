06f89a9b449e6da059063d637d8a2f386ca9f0e4
DiegoDiaz <diegodiazmed93@gmail.com>
2024-07-19 14:06:47 -0400

    test

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,15 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-website= "https://subslikescript.com/movie/Titanic-120338"
+website= "https://toscrape.com/"
 result=requests.get(website)
 
 content= result.text
 soup= BeautifulSoup(content,'lxml')
 
-box = soup.find('article',class_='main-article')
+box = soup.find('div',class_='container')
 title = box.find('h1').get_text()
-transcript = box.find('div',class_='full-script').get_text(strip=True, separator=' ')
+transcript = box.find('div',class_='row').get_text(strip=True, separator=' ')
 print(title,transcript)
 
 with open(f'{title}.txt','w') as file:
